056f653483ac5cfa18aeac1f6e166db638ef4f3a
Novohudo <89519243+Novohudo@users.noreply.github.com>
2022-08-23 18:56:26 +0200

    Class-Component(basic)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
 	const [value,setValue] = useState('текст в инпуте')
 
 	return (
 		<div className="App">
-			<Counter/>
+			<ClassCounter/>
 		</div>
 	);
 }
