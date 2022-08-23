773dcd624fcdeaf33ce52dc51484fd2911c46628
Novohudo <89519243+Novohudo@users.noreply.github.com>
2022-08-23 18:26:14 +0200

    SetState + Input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      777
-    </div>
-  );
+	const [likes, setLikes] = useState(0);
+	const [value,setValue] = useState('текст в инпуте')
+	const increment = () => {
+		setLikes(likes + 1)
+	}
+	const decrement = () => {
+		setLikes(likes - 1)
+	}
+	return (
+		<div className="App">
+			<h1>{likes}</h1>
+			<h1>{value}</h1>
+			<input
+				type="text"
+				value={value}
+				onChange={event => setValue(event.target.value)}/>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
+	);
 }
 
 export default App;
