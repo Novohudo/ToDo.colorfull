7a93ca900866f2627c228da47fa08906b8298231
Novohudo <89519243+Novohudo@users.noreply.github.com>
2022-08-24 19:32:02 +0200

    Work with Form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
@@ -6,29 +6,41 @@ import MyInput from "./components/UI/input/MyInput";
 
 //rsc-снипед для создания функции
 //e.preventDefault()-предотвращает дефолтное поведение
+//useRef()-для доступа к дом дереву и извлечению значений
 
 function App() {
 	const [posts, setPosts] = useState([
-		{id: 1, title: 'Javascript', body: 'Description'},
-		{id: 2, title: 'Javascript 2', body: 'Description'},
-		{id: 3, title: 'Javascript 3', body: 'Description'}
-	])
-	const [title,setTitle] = useState('')
-	const addNewPost =(e)=>{
-		e.preventDefault()
-		console.log(title)
-	}
+		{id:1,title:'Javascript', body:'Description'},
+		{id:2,title:'Javascript2', body:'Description'},
+		{id:3,title:'Javascript3', body:'Description'}
+	]);
+	const [newPost, setNewPost] = useState({title:'', body:''});
+	//сокращаем код не создавая кучи хуков-используй мультизадачный хук
+	//создавая в хуке обьект для внедрения в стейт
+	// const [title, setTitle] = useState('');
+	// const [body, setBody] = useState('');
 
+	const addNewPost = (e) => {
+		e.preventDefault();
+		setPosts([...posts,{...newPost,id:Date.now()}])
+		setNewPost({ title:'',body:''})
+	}
 	return (
 		<div className="App">
 			<form>
 				{/*управляемая компонента*/}
 				<MyInput
-					value={title}
-					onChange={e=>setTitle(e.target.value)}
+					value={newPost.title}
+					onChange={e => setNewPost({...newPost, title: e.target.value})}
 					type={"text"}
 					placeholder={'название поста'}/>
-				<MyInput type={"text"} placeholder={'описание поста'}/>
+
+				{/*Неуправляемая компонента*/}
+				<MyInput
+					value={newPost.body}
+					onChange={e => setNewPost({...newPost, body: e.target.value})}
+					type={"text"}
+					placeholder={'описание поста'}/>
 				<MyButton onClick={addNewPost}>новый пост</MyButton>
 			</form>
 			<PostList posts={posts} title={'Посты про JS'}/>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,11 +5,13 @@ const PostList = ({posts,title}) => {
 	return (
 		<div>
 			<h1 style={{textAlign:'center'}}>{title}</h1>
-			{posts.map(post=>
-				<PostItem post={post} key={post.id}/>
+			{posts.map((post,index)=>
+				<PostItem number={index + 1} post={post} key={post.id}/>
 			)}
 		</div>
 	);
 };
 
 export default PostList;
+
+//формирование правильной нумерации- number={index + 1}
